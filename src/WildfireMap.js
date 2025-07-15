998b1fcde02b174fc4a3eee095504e7cb628bc80
Casey Trombley <caseytrombley@gmail.com>
2025-07-15 15:28:44 -0400

    fix warnings

--- src/WildfireMap.js
+++ src/WildfireMap.js
@@ -1,53 +1,42 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import { BitmapLayer } from '@deck.gl/layers';
+import { Map } from '@vis.gl/react-maplibre';
+import maplibregl from 'maplibre-gl';
 import { GeoJsonLayer } from '@deck.gl/layers';
-import { TileLayer } from '@deck.gl/geo-layers';
 
+// Initial map view (centered over the U.S.)
 const INITIAL_VIEW_STATE = {
     longitude: -100,
     latitude: 40,
     zoom: 4,
     pitch: 0,
-    bearing: 0,
+    bearing: 0
 };
 
 function WildfireMap({ data }) {
-    const tileLayer = new TileLayer({
-        id: 'osm-tiles',
-        data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        minZoom: 0,
-        maxZoom: 19,
-        tileSize: 256,
-        renderSubLayers: ({ tile }) => {
-            const { west, south, east, north } = tile.bbox;
-            return new BitmapLayer({
-                id: `tile-bitmap-${tile.x}-${tile.y}-${tile.z}`,
-                image: tile.data, // this is the tile URL
-                bounds: [west, south, east, north]
-            });
-        }
-    });
-
     const wildfireLayer = new GeoJsonLayer({
-        id: 'wildfire-points',
+        id: 'wildfire-layer',
         data,
         pickable: true,
+        getRadius: f => f.properties?.FRP || 10,
         pointRadiusMinPixels: 2,
         pointRadiusScale: 200,
-        getPointRadius: f => f.properties.FRP || 10,
-        getFillColor: [255, 140, 0, 200],
-        getLineColor: [255, 100, 0],
-        lineWidthMinPixels: 1
+        getFillColor: [255, 140, 0, 180], // orange/yellow fill
     });
 
     return (
         <DeckGL
             initialViewState={INITIAL_VIEW_STATE}
             controller={true}
-            layers={[tileLayer, wildfireLayer]}
-            style={{ position: 'absolute', width: '100%', height: '100%' }}
-        />
+            layers={[wildfireLayer]}
+        >
+            <Map
+                reuseMaps
+                mapLib={maplibregl}
+                mapStyle="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+                style={{ width: '100%', height: '100%' }}
+            />
+        </DeckGL>
     );
 }
 
